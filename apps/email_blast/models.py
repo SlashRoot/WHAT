d3b9be0277b5eb69245e769a927627c7ed65fc9f
rachelLagodka <rachel.lagodka@gmail.com>
2012-05-04 23:36:38 -0400

    cleanup on apps/email_blast/models.py

--- apps/email_blast/models.py
+++ apps/email_blast/models.py
@@ -1,16 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-class Group(models.Model):
-    name = models.CharField()
-    
-class RoleInGroup(models.Model):
-    name = models.CharField()
-    
-class UserInGroup(models.Model):
-    user = models.ForeignKey(User)
-    role = models.ForeignKey(RoleInGroup)
-    group = models.ForeignKey(Group)
-    
-    
+class BlastMessage(models.Model):
+    '''
+    This is the model whose fields will be a part of the Blast Form
+    '''
+    subject = models.CharField(max_length=60)
+    message = models.TextField()
+    role = models.ForeignKey('people.RoleInGroup')
+    group = models.ForeignKey('people.Group')
+    send_to_higher_roles = models.BooleanField(default=True)    
