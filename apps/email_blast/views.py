6758d9d430f78025409567d2e0f77ab04044500a
dominick <dpiaquadio@gmail.com>
2012-05-15 13:25:45 -0400

    Manually overriding forms.py and views.py so I can work on tests and other things

--- apps/email_blast/views.py
+++ apps/email_blast/views.py
@@ -2,11 +2,35 @@
 from django.core.mail import send_mail
 from django.shortcuts import render
 
+from email_blast.forms import BlastMessageForm
+
 def email_blast(request):
-    try:
-        send_mail('Experiment One', 'Test.', 'glacierformdomino@hotmail.com',
-                  ['dpiaquadio@gmail.com', 'glacierformdomino@hotmail.com', 'darkcatholic619@aol.com'], fail_silently=False)
-    except:
-        pass
+    '''
+Whenever members in roles within a group need notifications, they will receive a message
+all at once in a blast instead of emailing to individual members within roles in a group.
+-Dominick
+'''
+    ## Step one: Gathering Data
+    if request.method == 'POST':
+        form = BlastMessageForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            role = form.cleaned_data['role']
+            group = form.cleaned_data['group']
+            send_to_higher_roles = form.cleaned_data['send_to_higher_roles']
+            
+            ## Step two: Piece the data together to form a message
+            blast_message = "To " + role + group + ":" + "\n" + message ## Result Example: To Keeper in Slashroot: (next line) Hello Keeper in Slashroot! How are you?
+            
+            ## Step three: Send the data out. Take notice that blast_message is being used rather than just message.
+            ##TODO: I've set up the structure for this form to work. Now we need to figure out how to gather email information because this will not work at all.
+            send_mail(subject, blast_message, 'dpiaquadio@gmail.com', group)
+            
+            return HTTPResponseRedirect('/blast_form/confirmation/')
+        else:
+            form = BlastMessageForm()
+    else:
+        form = BlastMessageForm()
     
     return render(request,'email_blast/email_blast_form.html',locals())
