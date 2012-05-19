c7c2d2a385e79b7daf4612959082c15220a44f1d
jMyles <justin@justinholmes.com>
2012-05-18 20:53:35 -0400

    re-thought runtime error

--- apps/comm/call_functions.py
+++ apps/comm/call_functions.py
@@ -58,21 +58,17 @@ def get_or_create_nice_number(incoming_number):
     '''
     A wrapper over get_or_create for a PhoneNumber object.  Gets or Creates a new PhoneNumber from various formats.
     '''
-    try:
-        number_as_list = re.findall(r"[0-9]", incoming_number) #No matter the format, grab only the numbers.
-        
-        #An unknown caller will produce a completely blank number.
-        if not number_as_list:
-            return PhoneNumber.objects.get_blank_number()
-        
-        if number_as_list[0] == '1': #If the first digit is a 1, we'll just pop it off.
-            number_as_list.pop(0)
-        if not len(number_as_list) == 10: #Now we expect to have exactly ten digits.
-            raise TypeError("I wasn't able to discern exactly ten digits from the phone number you gave me.")
-        incoming_number = ''.join(number_as_list) #Now our incoming number is properly formatted.
-        
-    except TypeError:
-        raise RuntimeError("We expected this phone number to be a string.  It wasn't.  You suck.")
+    number_as_list = re.findall(r"[0-9]", incoming_number) #No matter the format, grab only the numbers.
+    
+    #An unknown caller will produce a completely blank number.
+    if not number_as_list:
+        return PhoneNumber.objects.get_blank_number()
+    
+    if number_as_list[0] == '1': #If the first digit is a 1, we'll just pop it off.
+        number_as_list.pop(0)
+    if not len(number_as_list) == 10: #Now we expect to have exactly ten digits.
+        raise TypeError("I wasn't able to discern exactly ten digits from the phone number you gave me.  It was %s" % incoming_number)
+    incoming_number = ''.join(number_as_list) #Now our incoming number is properly formatted.
 
     phone_number = "+1" + str(incoming_number)
     nice_number = phone_number[2:5] + "-" + phone_number[5:8] + "-" + phone_number[8:] #parse the number to look like django wants it: ex. 845-633-8330
