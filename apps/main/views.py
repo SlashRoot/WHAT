29c55a00069791bbcb0f468e9324a9bc2cf61026
timgordon <lbsc_lead@yahoo.com>
2012-07-15 10:24:54 -0400

    lets move!

--- apps/main/views.py
+++ apps/main/views.py
@@ -104,4 +104,5 @@ def hack_and_tell(request):
     return render(request,'main/hack_and_tell/info.html', locals())
 
 def moving(request):
+    moving_content_blocks = ContentBlock.objects.filter(tags__name='moving_2012_blocks')
     return render(request, 'main/moving.html', locals())
