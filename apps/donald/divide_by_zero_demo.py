3b95f2dd27fe0d063bfad36ada7c241a9f0378f2
jMyles <justin@justinholmes.com>
2012-10-19 18:54:54 -0400

    Divide by Zero examination from workout 10/19

---
+++ apps/donald/divide_by_zero_demo.py
@@ -0,0 +1,12 @@
+def function1(x):
+	return 97 / x
+
+def function2(w):
+    for somenumber in [w..0]:
+        changingint = 0
+        while changingint < w:
+            sumnumber = changingint + somenumber
+            print("%s plus %s equals %s" % (changingint, somenumber, sumnumber))
+            changingint = sumnumber
+
+	print("%s goes into %s %s times with a remainder of %s" % (somenumber, w, quotient, remainder)
