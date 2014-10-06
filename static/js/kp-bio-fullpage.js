f79ed89a789a6242ca0e78e159cf86f739ca9f81
Kris Laratta <krislaratta@gmail.com>
2014-10-06 03:08:13 -0400

    Working fullpage.js and bootstrap implementation

---
+++ static/js/kp-bio-fullpage.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $('#fullpage').fullpage({
+        verticalCentered: true,
+        resize : true,
+        sectionsColor : ['#ccc', 'blue'],
+        anchors:['firstSlide', 'secondSlide'],
+        scrollingSpeed: 700,
+        easing: 'easeInQuart',
+        menu: false,
+        navigation: false,
+        navigationPosition: 'right',
+        navigationTooltips: ['firstSlide', 'secondSlide'],
+        slidesNavigation: true,
+        slidesNavPosition: 'bottom',
+        loopBottom: false,
+        loopTop: false,
+        loopHorizontal: true,
+        autoScrolling: false,
+        scrollOverflow: false,
+        css3: false,
+        paddingTop: '3em',
+        paddingBottom: '10px',
+        normalScrollElements: '#element1, .element2',
+        normalScrollElementTouchThreshold: 5,
+        keyboardScrolling: true,
+        touchSensitivity: 15,
+        continuousVertical: false,
+        animateAnchor: true,
+        sectionSelector: '.section',
+        slideSelector: '.slide',
+
+        //events
+        onLeave: function(index, nextIndex, direction){},
+        afterLoad: function(anchorLink, index){},
+        afterRender: function(){},
+        afterResize: function(){},
+        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
+        onSlideLeave: function(anchorLink, index, slideIndex, direction){}
+    });
+});
