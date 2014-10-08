d046b0a7f4e714734605d98650e83d061b948956
KPrasch <kieranprasch@gmail.com>
2014-10-07 21:38:49 -0400

    restructured static files tree.

--- static/js/fullpage-settings.js
+++ static/js/fullpage-settings.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
     $('#fullpage').fullpage({
-        verticalCentered: false,
+        verticalCentered: true,
         sectionsColor : ['#ccc', '#88abaf', '#99b089', '#b08d89'],
         anchors:[],
         scrollingSpeed: 600,
         navigation: true,
         navigationPosition: 'right',
         navigationTooltips: ['Home', 'Development','Networking', 'Cafe & Membership'],
-        slidesNavigation: false,
+        slidesNavigation: true,
         slidesNavPosition: 'bottom',
         css3: false,
         paddingTop: '3em',
