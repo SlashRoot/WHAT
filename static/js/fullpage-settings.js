f84693384c3f904fbbb27cec76c9df75240a9b5b
KPrasch <kieranprasch@gmail.com>
2014-10-10 21:57:02 -0400

    styling for the new layout.

--- static/js/fullpage-settings.js
+++ static/js/fullpage-settings.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $("#header, #footer").animate({height:"10%"}, 2000, 'swing');
+    $("#header, #footer").animate({height:"8%"}, 2000, 'swing');
     $('#fullpage').fullpage({
         verticalCentered: false,
         sectionsColor : ['black', '#88abaf', '#99b089', '#b08d89'],
